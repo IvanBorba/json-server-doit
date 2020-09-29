dcf64f176587d40f99d4033a2fce0145502434dd
Filipe Gutierry <filipe@kenzie.com.br>
2020-09-29 14:59:01 -0300

    fix: added cors to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var cors = require('cors');
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 
@@ -11,6 +12,7 @@ const rules = auth.rewriter({
   "/users*": "/600/users$1"
 });
 
+app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
