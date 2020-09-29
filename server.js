e30a27aeec14f123a3a9329ccfc00f0024acfbf4
Filipe Gutierry <filipe@kenzie.com.br>
2020-09-29 10:55:21 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+const port = process.env.PORT || 3001;
+
+app.db = router.db;
+
+const rules = auth.rewriter({
+  "/users*": "/600/users$1"
+});
+
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port);
+
+console.log("Server is running on port:", port);
