e394fe64555019e2850c86b5f2a45baab9480b02
Gabriel Araújo <araujooj@MacBook-Air-de-Gabriel.local>
2021-09-01 09:35:07 -0300

    app

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var cors = require('cors');
+const cors = require("cors");
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3333;
 
 app.db = router.db;
 
 const rules = auth.rewriter({
   "/users*": "/600/users$1",
+  "/tasks*": "/600/tasks$1",
 });
 
 app.use(cors());
